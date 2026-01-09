e6748dbaf285c40871eb51bfac5a9b9a25ca79b9
maruma <maruyama_001@jimusuru.info>
2026-01-09 22:51:20 +0900

    Update key box unlock instructions

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -982,7 +982,7 @@ export default function MyPage() {
                                     className="h-20 w-20 rounded border border-gray-200 object-contain transition group-hover:scale-105"
                                   />
                                 </button>
-                                <div className="text-xs text-gray-600">鍵ボックスのリーダーにかざして解錠してください。</div>
+                                <div className="text-xs text-gray-600">ここを、QRをタップして、鍵ボックスのリーダーにかざして解錠してください。</div>
                               </div>
                             ) : null}
                           </div>
